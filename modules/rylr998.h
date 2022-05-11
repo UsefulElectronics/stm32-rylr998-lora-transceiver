84b95560b001c1c29f2966383dac921c711dc7a6
Ward Almasarani <wardzx@hotmail.com>
2022-05-12 00:09:47 +0300

    Module handler initialization has started

--- modules/rylr998.h
+++ modules/rylr998.h
@@ -35,6 +35,35 @@ typedef enum
 	Rylr998_TIMEOUT
 } Rylr998_Status_t;
 
+typedef union																	// module send to Lop
+{	uint8_t data;
+	struct
+	{
+		uint8_t B0				:1;
+		uint8_t B1				:1;
+		uint8_t B2				:1;
+		uint8_t B3				:1;
+		uint8_t B4				:1;
+		uint8_t B5				:1;
+		uint8_t B6				:1;
+		uint8_t B7				:1;
+	}bit;
+
+}Flag8_t;
+
+typedef struct
+{
+	bool 				rylr998Reset;
+	bool 				rylr998RxBuffer;
+	bool				rylr998TxBuffer;
+	uint8_t				rylr998Timer;
+	Flag8_t				rylr998Flag;
+
+	uint16_t			indicatorAddress;
+	int16_t				indicatorCounter;
+	uint8_t 			indicatorFlasher;
+}Rylr998Handler_t;
+
 
 
 void rylr998_disable				(void);
