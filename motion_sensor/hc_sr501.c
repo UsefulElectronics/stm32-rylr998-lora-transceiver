365a4ce51faf5ea82343ddd3a9caa756c4dd9f59
Ward Almasarani <wardzx@hotmail.com>
2022-06-28 23:29:14 +0300

    Most of the required motion sensor handling funcitons has been added

--- motion_sensor/hc_sr501.c
+++ motion_sensor/hc_sr501.c
@@ -13,7 +13,6 @@
 
 /* INCLUDES ------------------------------------------------------------------*/
 #include "hc_sr501.h"
-
 /* PRIVATE STRUCTRES ---------------------------------------------------------*/
 
 /* VARIABLES -----------------------------------------------------------------*/
@@ -25,6 +24,78 @@
 /* PRIVATE FUNCTIONS DECLARATION ---------------------------------------------*/
 
 /* FUNCTION PROTOTYPES -------------------------------------------------------*/
-
-
+/**
+ * @brief
+ *
+ * @param hMotionSens
+ */
+void hc_sr501Init(hc_sr501_t* hMotionSens)
+{
+	hMotionSens->sesorActivation = true;
+}
+/**
+ * @brief
+ *
+ * @param hMotionSens
+ * @return
+ */
+bool hc_sr501ReadStatus(hc_sr501_t* hMotionSens)
+{
+	return hMotionSens->sesorStatus;
+}
+/**
+ * @brief
+ *
+ * @param hMotionSens
+ * @param sensorOutputStatus
+ */
+void hc_sr501UpdateStatus(hc_sr501_t* hMotionSens, uint8_t sensorOutputStatus)
+{
+	hMotionSens->sesorStatus = sensorOutputStatus;
+}
+/**
+ * @brief
+ *
+ * @param hMotionSens
+ */
+void hc_sr501ResetTimer(hc_sr501_t* hMotionSens)
+{
+	hMotionSens->sensorTimer = 0;
+}
+/**
+ * @brief
+ *
+ * @param hMotionSens
+ * @return
+ */
+uint32_t hc_sr501ReadTimer(hc_sr501_t* hMotionSens)
+{
+	return hMotionSens->sensorTimer;
+}
+/**
+ * @brief
+ *
+ * @param hMotionSens
+ * @param currentTickValue
+ */
+void hc_sr501UpdateTimer(hc_sr501_t* hMotionSens,uint32_t currentTickValue)
+{
+	hMotionSens->sensorTimer = currentTickValue;
+}
+/**
+ * @brief
+ *
+ * @param hMotionSens
+ * @param timeout
+ */
+bool boolhc_sr501CheckTimer(hc_sr501_t* hMotionSens, uint32_t currentTickValue, uint32_t timeout)
+{
+	bool ret = false;
+	ret = currentTickValue - hMotionSens->sensorTimer >= timeout ? true : false;
+	if(ret)
+	{
+		hc_sr501UpdateTimer(hMotionSens, currentTickValue);
+	}
+	return ret;
+}
 /**************************  Useful Electronics  ****************END OF FILE***/
--- motion_sensor/hc_sr501.h
+++ motion_sensor/hc_sr501.h
@@ -14,17 +14,32 @@
 #define HC_SR501_H_
 
 
-/* INCLUDES ------------------------------------------------------------------*/
-
+/* INCLUDES ----------------------------------------- ------------------------*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 /* MACROS --------------------------------------------------------------------*/
-
+#define HC_SR501_DETECTION_TIME_HIGH	3		//3ms
+#define HC_SR501_BLOCK_TIME_LOW			6		//3ms
 /* ENUMORATIONS --------------------------------------------------------------*/
 
 /* STRUCTURES & TYPEDEFS -----------------------------------------------------*/
-
+typedef struct
+{
+	uint32_t 	sensorTimer;
+	bool		sesorStatus;
+	bool		sesorActivation;
+}hc_sr501_t;
 /* VARIABLES -----------------------------------------------------------------*/
 
 /* FUNCTIONS DECLARATION -----------------------------------------------------*/
+void 		hc_sr501Init			(hc_sr501_t* hMotionSens);
+bool 		hc_sr501ReadStatus		(hc_sr501_t* hMotionSens);
+void 		hc_sr501UpdateStatus	(hc_sr501_t* hMotionSens, uint8_t sensorOutputStatus);
+void 		hc_sr501ResetTimer		(hc_sr501_t* hMotionSens);
+uint32_t	hc_sr501ReadTimer		(hc_sr501_t* hMotionSens);
+void 		hc_sr501UpdateTimer		(hc_sr501_t* hMotionSens,uint32_t currentTickValue);
+bool 		boolhc_sr501CheckTimer	(hc_sr501_t* hMotionSens, uint32_t currentTickValue, uint32_t timeout);
 
 
 
